51b9cf4c370f3961a24b49e22f92e7794437d3e9
yifatre <yifat.reg@gmail.com>
2024-02-13 18:18:16 +0200

    Improve onThirdBallClick()

--- js/main.js
+++ js/main.js
@@ -16,17 +16,18 @@ function onThirdBallClick() {
     const color1 = getComputedStyle(elBall1).getPropertyValue('background-color')
     const color2 = getComputedStyle(elBall2).getPropertyValue('background-color')
 
-    const height1 = getComputedStyle(elBall1).getPropertyValue('height')
-    const height2 = getComputedStyle(elBall2).getPropertyValue('height')
+    const radius1 = getComputedStyle(elBall1).getPropertyValue('height')
+    const radius2 = getComputedStyle(elBall2).getPropertyValue('height')
     
-    const width1 = getComputedStyle(elBall1).getPropertyValue('width')
-    const width2 = getComputedStyle(elBall2).getPropertyValue('width')
-    
-    elBall1.style.width = width2
-    elBall1.style.height = height2
+    elBall1.style.width = radius2
+    elBall1.style.height = radius2
     elBall1.style.backgroundColor = color2
+    elBall1.innerText = radius2.slice(0, -2)
   
-    elBall2.style.width = width1
-    elBall2.style.height = height1
+    elBall2.style.width = radius1
+    elBall2.style.height = radius1
     elBall2.style.backgroundColor = color1
+    elBall2.innerText = radius1.slice(0, -2)
+}
+
 }
