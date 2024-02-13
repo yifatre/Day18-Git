c3a5c664d1b2204355fe103dd6096c5683a1cbb4
yifatre <yifat.reg@gmail.com>
2024-02-13 16:42:19 +0200

    Change ball to a random color

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 'use strict'
 
 function onBallClick(elBall) {
-    var ballRadius = +elBall.innerText + 50
+    var ballRadius = +elBall.innerText + getRandomIntInclusive(20, 60)
     if (ballRadius > 400) ballRadius = 100
     elBall.style.width = ballRadius + 'px'
     elBall.style.height = ballRadius + 'px'
     elBall.innerText = ballRadius
+    elBall.style.backgroundColor = getRandomColor()
 }
