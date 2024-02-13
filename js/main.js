cbd5af170d6335715e289ee5f0da6ac5772e306a
yifatre <yifat.reg@gmail.com>
2024-02-13 18:18:45 +0200

    Add fourth ball

--- js/main.js
+++ js/main.js
@@ -18,16 +18,31 @@ function onThirdBallClick() {
 
     const radius1 = getComputedStyle(elBall1).getPropertyValue('height')
     const radius2 = getComputedStyle(elBall2).getPropertyValue('height')
-    
+
     elBall1.style.width = radius2
     elBall1.style.height = radius2
     elBall1.style.backgroundColor = color2
     elBall1.innerText = radius2.slice(0, -2)
-  
+
     elBall2.style.width = radius1
     elBall2.style.height = radius1
     elBall2.style.backgroundColor = color1
     elBall2.innerText = radius1.slice(0, -2)
 }
 
+function onFourthBallClick() {
+    const elBall1 = document.querySelector('.ball1')
+    const elBall2 = document.querySelector('.ball2')
+
+    var radius1 = +elBall1.innerText - getRandomIntInclusive(20, 60)
+    if (radius1 < 100) radius1 = 100
+    elBall1.style.width = radius1 + 'px'
+    elBall1.style.height = radius1 + 'px'
+    elBall1.innerText = radius1
+    
+    var radius2 = +elBall1.innerText - getRandomIntInclusive(20, 60)
+    if (radius2 < 100) radius2 = 100
+    elBall2.style.width = radius2 + 'px'
+    elBall2.style.height = radius2 + 'px'
+    elBall2.innerText = radius2
 }
