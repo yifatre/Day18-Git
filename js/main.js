c9fb71d3be3f5106fa28ebf612c867ddf5593703
yifatre <yifat.reg@gmail.com>
2024-02-13 16:21:47 +0200

    Add ball size limit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 
 function onBallClick(elBall) {
     var ballRadius = +elBall.innerText + 50
+    if (ballRadius > 400) ballRadius = 100
     elBall.style.width = ballRadius + 'px'
-    elBall.style.height = ballRadius  + 'px'
+    elBall.style.height = ballRadius + 'px'
     elBall.innerText = ballRadius
 }
